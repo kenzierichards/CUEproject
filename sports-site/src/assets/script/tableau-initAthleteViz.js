7f32ba8c7519c77cb41c8d504f05cef216921a3c
kenzierichards <49963787+kenzierichards@users.noreply.github.com>
2021-05-21 11:32:23 -0400

    Update tableau-initAthleteViz.js

--- sports-site/src/assets/script/tableau-initAthleteViz.js
+++ sports-site/src/assets/script/tableau-initAthleteViz.js
@@ -1,39 +1,26 @@
 function initAthleteViz() {
-
+    //grab page element
     var containerDiv = document.getElementsByTagName('app-athletes-page').item(0).getElementsByTagName('div').item(0),
-  
+    
+    //Tableau public visualization    
     url = "https://public.tableau.com/views/Trackv2/StudentTable?:language=en&:display_count=y&:origin=viz_share_link",
-  
+        
     options = {
+      height: 800,
+      width: 1000,
+      hideTabs: true,
   
-    height: 800,
-  
-    width: 1000,
-  
-    hideTabs: true,
-  
+    //debugging
     onFirstInteractive: function () {
-  
-    console.log("Run this code when the viz has finished loading.");
-  
-    }
-  
+      console.log("Visualization made");
+      }
     };
   
-   
-  
     if(viz != null) {
-  
-    viz.dispose();
-  
+      viz.dispose();
     }
-  
    
-  
+    //create a new object, put in container
     var viz = new tableau.Viz(containerDiv, url, options);
-  
-    // Create a viz object and embed it in the container div.
-  
-   
   
   }
